2010966bc12e20abc0f5148a939d2f852e3fd5f6
alex <alexcarvajal2404@gmail.com>
2023-01-10 13:35:18 -0500

    :zap: tawk component for chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import React from 'react';
 import ThemeConfig from "@/styles/theme";
 import GlobalStyles from "@/styles/theme/globalStyles";
 import ScrollToTop from "@/shared/components/ScrollToTop";
 import Router from "@/routes";
 import {SnackbarProvider} from 'notistack'
+// @ts-ignore
+import TawkTo from 'tawkto-react'
+
+const propertyId = '63bdab1947425128790cb8cf';
+const tawkId = '1gmee8ng9';
 
 
 function App() {
+  React.useEffect(() => {
+    const tawk = new TawkTo(propertyId, tawkId)
+
+    tawk.onStatusChange((status: any) =>
+    {
+      console.log('status', status)
+    })
+  }, [])
+
+
   return (
     <ThemeConfig>
       <GlobalStyles />
